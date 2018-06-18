98dd787dde837cec5090432cea265ea4beb17dd3
solianri <deeper1@163.com>
2018-06-18 16:35:21 +0800

    xxx

--- netease/neteaseCrawler.py
+++ netease/neteaseCrawler.py
@@ -7,38 +7,36 @@
 """
 
 import json
+import csv
+import os
 import requests
-import csv,json
-import pandas as pd
 
 
 class neteaseCrawler():
     def __init__(self):
         return
 
-    def requestcsv(self, code):
+    def requestJson(self, code):
+        def loadjson():
+            cf = open(filename, 'r')
+            results = []
+            for x in csv.DictReader(cf):
+                d = json.dumps(x, indent=4, separators=(',', ':'), ensure_ascii=False)  # ,sort_keys=True
+                results.append(d)
+            cf.close()
+            return results
+
         filename = '../Data/' + code + '.csv'
         url = "http://quotes.money.163.com/service/chddata.html?code=0" + code + "&start=20071105&end=20150618&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP"
-        response = requests.get(url=url)
+        response = requests.get(url = url)
         with open(filename, 'w') as f:
             f.write(response.content.decode('gb2312').encode('utf-8'))
         f.close()
-
-    def loadjson(self, code):
-        filename = '../Data/' + code + '.csv'
-        cf = open(filename, 'r')
-        for x in csv.DictReader(cf):
-            d = json.dumps(x, indent=4, separators=(',', ':'), ensure_ascii=False)  # ,sort_keys=True
-            print d
-        cf.close()
-
-        # t1 = pd.read_csv(filename)
-        # print t1.to_json()
-
+        results = loadjson()
+        os.remove(filename)
 
 # netease
 n = neteaseCrawler()
-n.requestcsv('601857')
-n.loadjson('601857')
+n.requestJson('601857')
 
 
