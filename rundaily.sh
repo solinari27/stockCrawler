7cd55c382eb8996ee42d308e9071a5646460e32b
solianri <deeper1@163.com>
2018-08-26 16:33:17 +0800

    sohu scrapy crawler

--- rundaily.sh
+++ rundaily.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 cd /home/solinari/workspace/stockCrawler
 #python /home/ubuntu/stockCrawler/crawlliangyeedaily.py&
 python /home/solinari/workspace/stockCrawler/crawlnetease.py&
--- runweekend.sh
+++ runweekend.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 /home/ubuntu/stockCrawler
 python /home/ubuntu/stockCrawler/crawlstockcode.py&
 sleep 1h
