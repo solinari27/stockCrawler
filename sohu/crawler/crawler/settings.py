a49bf2c93ed3b97e3fae9ae3713c700caca453ec
solinari <solinari27@gmail.com>
2018-08-28 21:20:34 +0800

    fix browser header
    Signed-off-by: solinari <solinari27@gmail.com>

--- sohu/crawler/crawler/settings.py
+++ sohu/crawler/crawler/settings.py
@@ -45,8 +45,7 @@ DEFAULT_REQUEST_HEADERS = {
     "Accept-Language": "en-US,en;q=0.8,zh-TW;q=0.6,zh;q=0.4",
     "Connection": "keep-alive",
     "Content-Type":" application/x-www-form-urlencoded; charset=UTF-8",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
-    "Referer": "http://www.zhihu.com/"
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36"
     }
 #DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
