6294ddcb0504f3dd7ab50e308b0735586b6059b6
solinari <solinari27@gmail.com>
2018-08-30 07:09:31 +0800

    manage mongo connection
    Signed-off-by: solinari <solinari27@gmail.com>

--- sohu/crawler.py
+++ sohu/crawler.py
@@ -9,6 +9,9 @@
 import sys
 import os
 
+from common.mongo.sohuConn import SohuConn
+
 sys.path.append(os.getcwd())
 os.system("scrapy runspider crawler/crawler/spiders/sohuCrawler.py --loglevel ERROR")
 
+conn = SohuConn("Conf/sohu.conf")
