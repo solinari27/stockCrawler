e78e5aebf014e2ade470cb7ed4ebda2affbf5db3
solinari <solinari27@gmail.com>
2018-08-29 20:05:27 +0800

    start scrapy sohu
    Signed-off-by: solinari <solinari27@gmail.com>

--- sohu/crawler/crawler/spiders/sohuCrawler.py
+++ sohu/crawler/crawler/spiders/sohuCrawler.py
@@ -8,6 +8,18 @@
 import scrapy
 
 class Sohu_crawler(scrapy.Spider):
+    def get_url(code=None, start=None, end=None):
+        url = "http://q.stock.sohu.com/hisHq?code=cn_" + str (code) + \
+              "&start=20130930&end=20131231&stat=1&order=D&period=d&callback=historySearchHandler&rt=jsonp"
+        return url
+
     name = "SohuCrawler"
-    start_urls = ['http://q.stock.sohu.com/hisHq?code=cn_300228&start=20130930&end=20131231&stat=1&order=D&period=d&callback=historySearchHandler&rt=jsonp']
+    allowed_domains = ["www.sohu.com"]
+    start_urls = []
+    start_urls = start_urls.append(get_url(code=300228))
+    # start_urls = ["http://q.stock.sohu.com/hisHq?code=cn_300228&start=20130930&end=20131231&stat=1&order=D&period=d&callback=historySearchHandler&rt=jsonp"]
+
+    def parse(self, response):
+        print response.body
+
 
