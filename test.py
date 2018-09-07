ef9fa3b1cc1ab3a8edae45a6e1727b7cc8ad0cb2
solianri <deeper1@163.com>
2018-09-07 21:32:15 +0800

    sohu scrapy crawler

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import os
 from common.mongo.sohuConn import SohuConn
 
 sys.path.append(os.getcwd())
-# os.system("scrapy runspider crawler/crawler/spiders/sohuCrawler.py --loglevel ERROR")
+os.system("scrapy runspider sohu/crawler/crawler/spiders/sohuCrawler.py --loglevel ERROR")
 
 conn = SohuConn("/home/solinari/workspace/stockCrawler/Conf/sohu.conf")
+print conn.getStocks()
