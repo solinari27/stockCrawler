54589b1a688a2face802e0df7f0333d00388723a
solinari27 <solinari27@gmail.com>
2018-09-07 21:51:52 +0800

    sohu scrapy crawler

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ from common.mongo.sohuConn import SohuConn
 sys.path.append(os.getcwd())
 os.system("scrapy runspider sohu/crawler/crawler/spiders/sohuCrawler.py --loglevel ERROR")
 
-conn = SohuConn("/home/solinari/workspace/stockCrawler/Conf/sohu.conf")
-print conn.getStocks()
+# conn = SohuConn("/home/solinari/workspace/stockCrawler/Conf/sohu.conf")
+# print conn.getStocks()
