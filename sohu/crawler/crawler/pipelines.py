8c8633408b4feba4e7f732a82bebee7d8c964fd6
solinari <solinari27@gmail.com>
2018-09-09 22:09:55 +0800

    enable pipeline to parse data
    Signed-off-by: solinari <solinari27@gmail.com>

--- sohu/crawler/crawler/pipelines.py
+++ sohu/crawler/crawler/pipelines.py
@@ -9,9 +9,11 @@ import json
 
 class CrawlerPipeline(object):
     def process_item(self, item, spider):
+        url = item.get ("url")
+        code = item.get ("code")
+        date = item.get ("date")
         raw_data = item.get("raw_data")
-        url = item.get("url")
-        code = item.get("code")
-        date = item.get("date")
 
+        for data in raw_data["hq"]:
+            print data
         # return item
