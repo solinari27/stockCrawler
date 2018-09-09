eac0768eb93d3acca4598ea1615dc923c5bd77b0
solinari <solinari27@gmail.com>
2018-09-09 22:20:46 +0800

    enable pipeline to parse data
    Signed-off-by: solinari <solinari27@gmail.com>

--- sohu/crawler/crawler/pipelines.py
+++ sohu/crawler/crawler/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import json
+from common.mongo.sohuConn import SohuConn
 
 class CrawlerPipeline(object):
     def process_item(self, item, spider):
@@ -13,7 +13,10 @@ class CrawlerPipeline(object):
         code = item.get ("code")
         date = item.get ("date")
         raw_data = item.get("raw_data")
+        sohuconn = SohuConn("/home/solinari/workspace/stockCrawler/Conf/sohu.conf")
+        sohuconn.set_name("stock_" + str(code) + " pipeline")
 
         for data in raw_data["hq"]:
             print data
+            # add to mongo
         # return item
