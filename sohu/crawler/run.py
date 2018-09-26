557f27141829fa030e9b5ef23050aee11ed11942
solinari <solinari27@gmail.com>
2018-09-26 20:44:45 +0800

    netease crawler bugfix
    Signed-off-by: solinari <solinari27@gmail.com>

--- sohu/crawler/run.py
+++ sohu/crawler/run.py
@@ -3,5 +3,5 @@ import os
 
 sys.path.append(os.getcwd())
 # os.system("scrapy runspider sohu/crawler/crawler/spiders/sohuCrawler.py --loglevel ERROR")
-os.system("export PYTHONPATH=/home/solinari/workspace/stockCrawler:%PYTHONPATH")
+os.system("export PYTHONPATH=/home/ubuntu/stockCrawler:/home/solinari/workspace/stockCrawler:%PYTHONPATH")
 os.system("scrapy crawl SohuCrawler ")
