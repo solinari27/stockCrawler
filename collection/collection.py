ad96c8e8cfb6bf750266d8fa60f08b27e65a1eb8
solinari <solinari27@gmail.com>
2018-11-04 21:20:24 +0800

    format data Signed-off-by: solinari <solinari27@gmail.com>

--- collection/collection.py
+++ collection/collection.py
@@ -54,37 +54,54 @@ class Collection(object):
 
             for key in netease_item.keys():
                 for case in switch(key):
-                  if case('TCAP'):
-                    print "get TCAP", netease_item[key]
-                    break
+
+
                   if case('HIGH'):
+                    item[key] = netease_item[key]
                     print "get HIGH", netease_item[key]
                     break
                   if case('LOW'):
+                    item[key] = netease_item[key]
                     print "get LOW", netease_item[key]
                     break
+
+                #drop this
                   if case('CODE'):
-                    print "get CODE", netease_item[key]
+                    # print "get CODE", netease_item[key]
                     break
                   if case('DATE'):
-                    print "get DATE", netease_item[key]
+                    # print "get DATE", netease_item[key]
                     break
                   if case('NAME'):
-                    print "get NAME", netease_item[key]
+                    # print "get NAME", netease_item[key]
                     break
-                  if case('CHG'):
-                    print "get CHG", netease_item[key]
+                  if case('_id'):
+                      break
+
+                  if case('TCAP'):
+                    item[key] = netease_item[key]
+                    print "get TCAP", netease_item[key]
                     break
                   if case('MCAP'):
+                    item[key] = netease_item[key]
                     print "get MCAP", netease_item[key]
                     break
+
+                  if case('CHG'):
+                    item[key] = netease_item[key]
+                    print "get CHG", netease_item[key]
+                    break
                   if case('PCHG'):
+                    item[key] = netease_item[key]
                     print "get PCHG", netease_item[key]
                     break
+
                   if case('LCLOSE'):
+                    item[key] = netease_item[key]
                     print "get LCLOSE", netease_item[key]
                     break
                   if case('TOPEN'):
+                    item[key] = netease_item[key]
                     print "get TOPEN", netease_item[key]
                     break
                   if case('TCLOSE'):
@@ -93,14 +110,17 @@ class Collection(object):
                   if case('VATURNOVER'):
                     print "get VATURNOVER", netease_item[key]
                     break
+
                   if case('VOTURNOVER'):
+                    item[key] = netease_item[key]
                     print "get VOTURNOVER", netease_item[key]
                     break
                   if case('TURNOVER'):
+                    item[key] = netease_item[key]
                     print "get TURNOVER", netease_item[key]
                     break
                   if case(): # default, could also just omit condition or 'if True'
-                    print "something else!"
+                    pass
                     # No need to break here, it'll stop anyway
 
 
