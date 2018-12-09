c9ffd3afa8b548afebedec0fef4f3f39863c3e0a
solinari <solinari27@gmail.com>
2018-12-09 12:53:20 +0800

    add requirements support

---
+++ netease/crawler/crawler/spiders/neteaseCrawler.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# @Author  : Solinari
+# @Email   : 
+# @File    : neteaseCrawler.py
+# @Software: PyCharm
+# @Time    : 2018/12/09
+
+import os
+import time
+import json
+import yaml
+import scrapy
+
+
+class NeteaseCrawler(scrapy.Spider):
+    name = "NeteaseCrawler"
+    allowed_domains = ["www.163.com"]
+
+    def __load_conf(self):
+        yamlPath = os.path.join(os.getcwd(), 'spider.yaml')
+
+        f = open(yamlPath, 'r')
+        self._yamlconf = yaml.load(f.read())  # 用load方法转字典
+
+    def start_requests(self):
+        """
+        start with url in mongodb!!!
+        :return:
+        """
+        self.__load_conf()
+        headers = {}
+        cookies = {}
+        # conn = SohuConn(self._yamlconf['netease']['conf'])
+        # stocklist = conn.getStocks()
+
+
+    def parse(self, response):
+        print 'parse'
