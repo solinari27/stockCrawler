0f797d8a71ea4689ad73c77af0efcd3dede26e8b
solinari <solinari27@gmail.com>
2018-12-09 13:19:19 +0800

    use scrapy to crawl netease

--- netease/crawler/crawler/spiders/neteaseCrawler.py
+++ netease/crawler/crawler/spiders/neteaseCrawler.py
@@ -28,7 +28,34 @@ class NeteaseCrawler(scrapy.Spider):
         start with url in mongodb!!!
         :return:
         """
+        def do_init():
+            self._keylist = {}
+            self._keylist[u'\u6da8\u8dcc\u989d'] = 'CHG'
+            self._keylist[u'\u80a1\u7968\u4ee3\u7801'] = 'CODE'
+            self._keylist[u'\u603b\u5e02\u503c'] = 'TCAP'
+            self._keylist[u'\u524d\u6536\u76d8'] = 'LCLOSE'
+            self._keylist[u'\u6700\u9ad8\u4ef7'] = 'HIGH'
+            self._keylist[u'\u6d41\u901a\u5e02\u503c'] = 'MCAP'
+            self._keylist[u'\u6700\u4f4e\u4ef7'] = 'LOW'
+            self._keylist[u'\u6210\u4ea4\u91cf'] = 'VOTURNOVER'
+            self._keylist[u'\u65e5\u671f'] = 'DATE'
+            self._keylist[u'\u6362\u624b\u7387'] = 'TURNOVER'
+            self._keylist[u'\u540d\u79f0'] = 'NAME'
+            self._keylist[u'\u6da8\u8dcc\u5e45'] = 'PCHG'
+            self._keylist[u'\u5f00\u76d8\u4ef7'] = 'TOPEN'
+            self._keylist[u'\u6536\u76d8\u4ef7'] = 'TCLOSE'
+            self._keylist[u'\u6210\u4ea4\u91d1\u989d'] = 'VATURNOVER'
+
+            s = time.localtime(time.time())
+            year = s.tm_year
+            mon = s.tm_mon
+            day = s.tm_mday
+            # end date is today
+            enddate = str(year) + '%02d' % (mon) + '%02d' % (day)
+            return enddate
+
         self.__load_conf()
+        enddate = do_init()
         headers = {}
         cookies = {}
         conn = NeteaseConn(self._yamlconf['netease']['conf'])
@@ -37,7 +64,10 @@ class NeteaseCrawler(scrapy.Spider):
         for stock in stocklist:
             code = stock[0]
             type = stock[1]
-            pass
+            startdate = self._conn.getTime(code)
+            # results = self._requestJson(type, code, startdate, enddate)
+            # yield this
+
 
         yield "xxx"
 
