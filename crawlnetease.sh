c3eefe1a2128505d09e8172c00e759bd48489c13
solinari <solinari27@gmail.com>
2018-12-22 21:59:16 +0800

    bugfix for netease crawler

--- crawlnetease.sh
+++ crawlnetease.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd /home/ubuntu/stockCrawler/sohu/crawler
+cd /home/ubuntu/stockCrawler/netease/crawler
 export PYTHONPATH=/home/ubuntu/stockCrawler:%PYTHONPATH
 #cd /home/solinari/workspace/stockCrawler/netease/crawler
 #export PYTHONPATH=/home/solinari/workspace/stockCrawler:%PYTHONPATH
