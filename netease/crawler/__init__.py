944a3ba2ab592eb38829556b3fe301e8c6008517
solinari <solinari27@gmail.com>
2019-01-05 21:11:16 +0800

    use scrapy to get netease data

---
+++ netease/crawler/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author  : Solinari
+# @Email   : 
+# @File    : __init__.py
+# @Software: PyCharm
+# @Time    : 2019/01/05
+
--- netease/crawler/crawler/pipelines.py
+++ netease/crawler/crawler/pipelines.py
@@ -4,8 +4,48 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+import os
+import yaml
+from common.mongo.neteaseConn import NeteaseConn
 
 
 class CrawlerPipeline(object):
+    def __load_conf(self):
+        yamlPath = os.path.join(os.getcwd(), 'spider.yaml')
+
+        f = open(yamlPath, 'r')
+        self._yamlconf = yaml.load(f.read())  # 用load方法转字典
+
     def process_item(self, item, spider):
-        return item
+        def loadjson():
+            cf = open(file, 'r')
+            results = []
+            for x in csv.DictReader(cf):
+                d = json.dumps(x, indent=4, separators=(',', ':'), ensure_ascii=False)  # ,sort_keys=True
+                d2 = json.loads(d)
+                d3 = {}
+                for key in d2.keys():
+                    try:
+                        d3[self._keylist[key]] = float(d2[key])
+                    except:
+                        d3[self._keylist[key]] = d2[key]
+                results.append(d3)
+                # self._conn.insertDailyData(d2)
+            cf.close()
+            os.remove(file)
+            return results
+
+        url = item.get("url")
+        code = item.get("code")
+        date = item.get("date")
+        file = item.get("file")
+
+        self.__load_conf()
+        self.__conn = NeteaseConn(self._yamlconf['netease']['conf'])
+
+        result = loadjson()
+        for item in result:
+            item['CODE'] = code
+            self.__conn.insertDailyData(item)
+        self.__conn.updateTime(code, date)
