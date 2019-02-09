3c9594a65b61c5272c6800be4c5f71981dd3d98e
solinari <solinari27@gmail.com>
2019-02-09 12:16:47 +0800

    liangyee update

--- rundaily.sh
+++ rundaily.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/stockCrawler
-#python /home/ubuntu/stockCrawler/crawlliangyeedaily.py&
-#python /home/ubuntu/stockCrawler/crawlnetease.py&
-sh crawlnetease.sh
-sh crawlsohu.sh
+python /home/ubuntu/stockCrawler/crawlliangyeedaily.py
+# python /home/ubuntu/stockCrawler/crawlnetease.py&
+# sh crawlnetease.sh
+# sh crawlsohu.sh
