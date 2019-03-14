91b0a703216633ba84489fa28e68ae8ce6ca5697
solinari <solinari27@gmail.com>
2019-03-14 22:31:04 +0800

    add make collection part

---
+++ collection/make_collection.py
@@ -0,0 +1,32 @@
+#!usr/bin/env python
+# -*- coding:utf-8 _*-
+
+"""
+@author: solinari
+@file: make_collection.py
+@time: 2019/03/14
+"""
+import sys
+import time
+
+sys.path.append('/home/ubuntu/test')
+sys.path.append('/home/solinari/workspace/test')
+
+from collection import data_show
+from collection import collection
+from pytorch.sklearn_lineregression import do_regression
+
+
+c = collection.Collection()
+
+for result in c.getData(code="600000", start_date="2017-01-01", end_date="2018-12-31"):
+    ret = do_regression(result, epochs=10000, thres=10,
+                        DBSCAN_eps=3, DBSCAN_minsamples=4)
+    for item in ret:
+        w = item[0]
+        b = item[1]
+        dataset = result[item[2]: item[3]]
+        show = data_show.Plt()
+        show.load_data(data=dataset)
+        show.plot(w=w, b=b)
+        time.sleep(1)
---
+++ collection/utils/data_show.py
@@ -0,0 +1,60 @@
+#!usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@author: solinari
+@file: data_show.py
+@time: 2019/03/14
+"""
+import plotly.offline as pyoff
+import plotly.graph_objs as go
+import numpy as np
+
+
+class Plt(object):
+    """
+    show data with plotly
+    """
+
+    def __init__(self):
+        pass
+
+    def load_data(self, data):
+        self._data = data
+
+    def plot(self, **kwargs):
+        w, b = kwargs['w'], kwargs['b']
+        date = []
+        open = []
+        high = []
+        low = []
+        close = []
+        line_scatters = []
+        _x = 0
+        for item in self._data:
+            date.append(item['DATE'])
+            open.append(item['TOPEN'])
+            high.append(item['HIGH'])
+            low.append(item['LOW'])
+            close.append(item['TCLOSE'])
+            line_scatters.append(w * _x + b)
+            _x += 1
+
+        trace = go.Ohlc(x=date,
+                        open=open,
+                        high=high,
+                        low=low,
+                        close=close,
+                        increasing=dict(line=dict(color='red')),
+                        decreasing=dict(line=dict(color='green')),
+                        showlegend=False)
+
+        line = go.Scatter(
+            x=date,
+            y=line_scatters,
+            mode='lines+markers',
+            name='Regression',
+            showlegend=False,
+        )
+
+        data = [trace, line]
+        pyoff.plot(data)
