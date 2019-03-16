60e454eefd03dcfb4887a8cd068b4f6852fe4d93
solinari <solinari27@gmail.com>
2019-03-16 13:07:23 +0800

    bugfix

--- collection/collection.py
+++ collection/collection.py
@@ -1,19 +1,23 @@
 #!usr/bin/env python
-#-*- coding:utf-8 _*-
+# -*- coding:utf-8 _*-
 """
 @author: solinari
 @file: collection.py
 @time: 2018/10/09
 """
+import time
+
 from common.mongo.sohuConn import SohuConn
 from common.mongo.neteaseConn import NeteaseConn
 from utils.common.switch import switch
-import time
+
 
 class Collection(object):
     def __init__(self):
-        self.sohuconn = SohuConn("/home/solinari/workspace/stockCrawler/Conf/sohu.conf")
-        self.neteaseconn = NeteaseConn("/home/solinari/workspace/stockCrawler/Conf/netease.conf")
+        self.sohuconn = SohuConn(
+            "/home/solinari/workspace/stockCrawler/Conf/sohu.conf")
+        self.neteaseconn = NeteaseConn(
+            "/home/solinari/workspace/stockCrawler/Conf/netease.conf")
 
     def __del__(self):
         # self.sohuconn = None
@@ -32,107 +36,84 @@ class Collection(object):
         assert start_date is not None
         assert end_date is not None
 
-        netease_result = self.neteaseconn.getDailyData(code=str(code), date1=start_date, date2=end_date)
-        sohu_result = self.sohuconn.getDailyData(code=str(code), date1=start_date, date2=end_date)
+        netease_result = self.neteaseconn.getDailyData(
+            code=str(code), date1=start_date, date2=end_date)
+        sohu_result = self.sohuconn.getDailyData(
+            code=str(code), date1=start_date, date2=end_date)
         # assert (len(netease_result) == len(sohu_result))
 
         result = []
+        name = None
         for index, sohu_item in enumerate(sohu_result):
             netease_item = netease_result[index]
+            if name is None:
+                name = netease_item['NAME']
+
+            if netease_item['NAME'] != name and name is not None:
+                yield result
+                result = []
+
             item = {}
-            # for key in sohu_item.keys():
-            #     item[key] = sohu_item[key]
-            # for key in netease_item.keys():
-            #     if not key in sohu_item.keys():
-            #         item[key] = netease_item[key]
-            #         print key, "netease: ", netease_item[key]
-            #     else:
-            #         # assert (sohu_item[key] == netease_item[key])
-            #         print key, "netease: ", netease_item[key], "soho: ", sohu_item[key]
-            #         # find problem
-            #         pass
 
             for key in netease_item.keys():
                 for case in switch(key):
+                    # base info
+                    if case('HIGH'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('LOW'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('CODE'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('DATE'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('NAME'):
+                        item[key] = netease_item[key]
+                        break
 
+                # drop this
+                    if case('_id'):
+                        break
 
-                  if case('HIGH'):
-                    item[key] = netease_item[key]
-                    break
-                  if case('LOW'):
-                    item[key] = netease_item[key]
-                    break
+                # advantage info
+                    if case('TCAP'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('MCAP'):
+                        item[key] = netease_item[key]
+                        break
 
-                #drop this
-                  if case('CODE'):
-                    break
-                  if case('DATE'):
-                    break
-                  if case('NAME'):
-                    break
-                  if case('_id'):
-                      break
+                    if case('CHG'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('PCHG'):
+                        item[key] = netease_item[key]
+                        break
 
-                  if case('TCAP'):
-                    item[key] = netease_item[key]
-                    break
-                  if case('MCAP'):
-                    item[key] = netease_item[key]
-                    break
+                    if case('LCLOSE'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('TOPEN'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('TCLOSE'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('VATURNOVER'):
+                        item[key] = netease_item[key]
+                        break
 
-                  if case('CHG'):
-                    item[key] = netease_item[key]
-                    break
-                  if case('PCHG'):
-                    item[key] = netease_item[key]
-                    break
-
-                  if case('LCLOSE'):
-                    item[key] = netease_item[key]
-                    break
-                  if case('TOPEN'):
-                    item[key] = netease_item[key]
-                    break
-                  if case('TCLOSE'):
-                    item[key] = netease_item[key]
-                    break
-                  if case('VATURNOVER'):
-                    item[key] = netease_item[key]
-                    break
-
-                  if case('VOTURNOVER'):
-                    item[key] = netease_item[key]
-                    break
-                  if case('TURNOVER'):
-                    item[key] = netease_item[key]
-                    break
-                  if case(): # default, could also just omit condition or 'if True'
-                    pass
-                    # No need to break here, it'll stop anyway
+                    if case('VOTURNOVER'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('TURNOVER'):
+                        item[key] = netease_item[key]
+                        break
+                    if case():  # default, could also just omit condition or 'if True'
+                        pass
+                        # No need to break here, it'll stop anyway
             result.append(item)
-        return result
-
-
-# sohuconn = SohuConn("/home/solinari/workspace/stockCrawler/Conf/sohu.conf")
-# neteaseconn = NeteaseConn("/home/solinari/workspace/stockCrawler/Conf/netease.conf")
-#
-# s = time.localtime(time.time())
-# year = s.tm_year
-# mon = s.tm_mon
-# day = s.tm_mday
-# #end date is today
-#
-# startdate = str(year-1) + '-%02d' % (mon) + '-%02d' % (day)
-# enddate = str(year) + '-%02d' % (mon) + '-%02d' % (day)
-# result = neteaseconn.getDailyData(code=str("600000"), date1=startdate, date2=enddate)
-# for item in result:
-#     print item
-
-# startdate = str(year-2) + '-%02d' % (mon) + '-%02d' % (day)
-# enddate = str(year-1) + '-%02d' % (mon) + '-%02d' % (day)
-# result = sohuconn.getDailyData(code=str("600000"), date1=startdate, date2=enddate)
-# for item in result:
-#     print item
-
-c = Collection()
-c.getData(code="600000", start_date="2016-01-01", end_date="2018-12-31")
+        yield result
--- collection/make_collection.py
+++ collection/make_collection.py
@@ -19,7 +19,7 @@ from sk_utils.sklearn_lineregression import do_regression
 
 c = collection.Collection()
 
-for result in c.getData(code="600000", start_date="2017-01-01", end_date="2018-12-31"):
+for result in c.getData(code="600000", start_date="2017-01-01", end_date="2019-12-31"):
     ret = do_regression(result, epochs=10000, thres=10,
                         DBSCAN_eps=3, DBSCAN_minsamples=4)
     for item in ret:
