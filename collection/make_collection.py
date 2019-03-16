495e83dfbf979257f6d7d2c15ad2ac6c6de59de6
solinari <solinari27@gmail.com>
2019-03-16 21:01:08 +0800

    use yaml config

--- collection/make_collection.py
+++ collection/make_collection.py
@@ -11,21 +11,25 @@ import time
 sys.path.append('/home/ubuntu/stockCrawler')
 sys.path.append('/home/solinari/workspace/stockCrawler')
 
+import yaml
 from sk_utils.sklearn_lineregression import do_regression
 from tools import data_show
 import collection
 
-c = collection.Collection()
+with open("/home/solinari/workspace/stockCrawler/collection/conf/conf.yaml") as f:
+    conf = yaml.load(f)
+    print conf
+    c = collection.Collection()
 
-for result in c.getData(code="600000", start_date="2017-01-01", end_date="2019-12-31"):
-    ret = do_regression(result, epochs=10000, thres=10,
-                        algo='DBSCAN',
-                        DBSCAN_eps=3, DBSCAN_minsamples=4)
-    for item in ret:
-        w = item[0]
-        b = item[1]
-        dataset = result[item[2]: item[3]]
-        show = data_show.Plt()
-        show.load_data(data=dataset)
-        show.plot(w=w, b=b)
-        time.sleep(1)
+    for result in c.getData(code="600000", start_date="2017-01-01", end_date="2019-12-31"):
+        ret = do_regression(result, epochs=10000, thres=10,
+                            algo=conf['algo']['name'],
+                            DBSCAN_eps=3, DBSCAN_minsamples=4)
+        for item in ret:
+            w = item[0]
+            b = item[1]
+            dataset = result[item[2]: item[3]]
+            show = data_show.Plt()
+            show.load_data(data=dataset)
+            show.plot(w=w, b=b)
+            time.sleep(1)
