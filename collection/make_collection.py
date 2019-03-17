3289f1ba777ff83a941f79db1e477d87db98b7d3
solinari <solinari27@gmail.com>
2019-03-17 08:51:02 +0800

    select dataset

--- collection/make_collection.py
+++ collection/make_collection.py
@@ -11,24 +11,31 @@ import time
 sys.path.append('/home/ubuntu/stockCrawler')
 sys.path.append('/home/solinari/workspace/stockCrawler')
 
-import yaml
-from sk_utils.sklearn_lineregression import do_regression
-from tools import data_show
 import collection
+from tools import data_show
+from sk_utils.sklearn_lineregression import do_regression
+import yaml
 
 with open("/home/solinari/workspace/stockCrawler/collection/conf/conf.yaml") as f:
     conf = yaml.load(f)
     c = collection.Collection()
 
     for result in c.getData(code="600000", start_date="2017-01-01", end_date="2019-12-31"):
-        ret = do_regression(result, epochs=10000, thres=10,
+        ret = do_regression(result, epochs=conf['epochs'], thres=conf['thres'],
                             algo=conf['algo']['name'], params=conf['algo'])
         for item in ret:
             w = item[0]
             b = item[1]
             dataset = result[item[2]: item[3]]
             dateperiod = len(dataset)
-            # show = data_show.Plt()
-            # show.load_data(data=dataset)
-            # show.plot(w=w, b=b)
-            # time.sleep(1)
+            stand = conf['collection']
+            if dateperiod >= stand['dateperiod'] and w >= stand['w_up']:
+                show = data_show.Plt()
+                show.load_data(data=dataset)
+                show.plot(w=w, b=b)
+                time.sleep(1)
+            if dateperiod >= stand['dateperiod'] and w <= stand['w_down']:
+                show = data_show.Plt()
+                show.load_data(data=dataset)
+                show.plot(w=w, b=b)
+                time.sleep(1)
