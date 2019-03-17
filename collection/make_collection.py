aabd31a1d167711931eae2892b1fe0c12990f4b4
solinari <solinari27@gmail.com>
2019-03-17 21:57:34 +0800

    bugfix

--- collection/make_collection.py
+++ collection/make_collection.py
@@ -16,6 +16,12 @@ from tools import data_show
 from sk_utils.sklearn_lineregression import do_regression
 import yaml
 
+def del_useless_info(info_dict):
+    del(info_dict['CODE'])
+    del(info_dict['NAME'])
+    del(info_dict['DATE'])
+    return info_dict
+
 with open("/home/solinari/workspace/stockCrawler/collection/conf/conf.yaml") as f:
     conf = yaml.load(f)
     c = collection.Collection()
@@ -29,15 +35,18 @@ with open("/home/solinari/workspace/stockCrawler/collection/conf/conf.yaml") as 
             dataset = result[item[2]: item[3]]
             dateperiod = len(dataset)
             stand = conf['collection']
-            if dateperiod >= stand['dateperiod'] and w >= stand['w_up']:
+            if dateperiod >= stand['dateperiod_up'] and w >= stand['w_up']:
+                for _item in dataset:
+                    _item = del_useless_info(_item)
+                    print _item
+                # show = data_show.Plt()
+                # show.load_data(data=dataset)
+                # show.plot(w=w, b=b)
+                # time.sleep(1)
+            if dateperiod >= stand['dateperiod_down'] and w <= stand['w_down']:
                 print dataset
                 # show = data_show.Plt()
                 # show.load_data(data=dataset)
                 # show.plot(w=w, b=b)
                 # time.sleep(1)
-            if dateperiod >= stand['dateperiod'] and w <= stand['w_down']:
-                print dataset
-                # show = data_show.Plt()
-                # show.load_data(data=dataset)
-                # show.plot(w=w, b=b)
-                # time.sleep(1)
+                pass
