ca6ec87897fc9b581e0e35260d2471ef47a66e7f
solinari27 <solinari27@gmail.com>
2019-03-24 16:34:28 +0800

    make collection into torch.tensor

--- collection/make_collection.py
+++ collection/make_collection.py
@@ -12,6 +12,7 @@ sys.path.append('/home/ubuntu/stockCrawler')
 sys.path.append('/home/solinari/workspace/stockCrawler')
 
 import numpy as np
+import torch
 import collection
 from tools import data_show
 from sk_utils.sklearn_lineregression import do_regression
@@ -50,12 +51,12 @@ def data2ndarray(dataset):
     _data = np.array(_data).T
     return _data
 
-def make():
+def make_training_tensor(code, start_date, end_date):
     with open("/home/solinari/workspace/stockCrawler/collection/conf/conf.yaml") as f:
         conf = yaml.load(f)
         c = collection.Collection()
 
-        for result in c.getData(code="600007", start_date="2015-01-01", end_date="2019-12-31"):
+        for result in c.getData(code=code, start_date=start_date, end_date=end_date):
             ret = do_regression(result, epochs=conf['epochs'], thres=conf['thres'],
                                 algo=conf['algo']['name'], params=conf['algo'])
             for item in ret:
@@ -81,5 +82,5 @@ def make():
                     # time.sleep(1)
                     yield _data
 
-for ret in make():
-    print ret
+for ret in make_training_tensor(code="600007", start_date="2015-01-01", end_date="2019-12-31"):
+    print torch.tensor(ret)
