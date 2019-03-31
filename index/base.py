ddd757400006316830d3e26c2198b401f8cdb398
solinari <solinari27@gmail.com>
2019-03-31 22:03:26 +0800

    start index cal part

---
+++ index/base.py
@@ -0,0 +1,117 @@
+#!usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@author: solinari
+@file: base.py
+@time: 2018/10/09
+"""
+import time
+import sys
+
+sys.path.append('/home/ubuntu/stockCrawler')
+sys.path.append('/home/solinari/workspace/stockCrawler')
+
+from common.mongo.sohuConn import SohuConn
+from common.mongo.neteaseConn import NeteaseConn
+from utils.common.switch import switch
+
+
+class Base(object):
+    def __init__(self):
+        self.sohuconn = SohuConn(
+            "/home/solinari/workspace/stockCrawler/Conf/sohu.conf")
+        self.neteaseconn = NeteaseConn(
+            "/home/solinari/workspace/stockCrawler/Conf/netease.conf")
+
+    def __del__(self):
+        # self.sohuconn = None
+        # self.neteaseconn = None
+        pass
+
+    def getToday(self):
+        s = time.localtime(time.time())
+        year = s.tm_year
+        mon = s.tm_mon
+        day = s.tm_mday
+        return year, mon, day
+
+    def getData(self, code, start_date, end_date):
+        assert code is not None
+        assert start_date is not None
+        assert end_date is not None
+
+        netease_result = self.neteaseconn.getDailyData(
+            code=str(code), date1=start_date, date2=end_date)
+        sohu_result = self.sohuconn.getDailyData(
+            code=str(code), date1=start_date, date2=end_date)
+
+        result = []
+        name = None
+        for index, netease_item in enumerate(netease_result):
+            item = {}
+
+            for key in netease_item.keys():
+                for case in switch(key):
+                    # base info
+                    if case('HIGH'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('LOW'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('CODE'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('DATE'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('NAME'):
+                        item[key] = netease_item[key]
+                        break
+
+                # drop this
+                    if case('_id'):
+                        break
+
+                # advantage info
+                    if case('TCAP'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('MCAP'):
+                        item[key] = netease_item[key]
+                        break
+
+                    if case('CHG'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('PCHG'):
+                        item[key] = netease_item[key]
+                        break
+
+                    if case('LCLOSE'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('TOPEN'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('TCLOSE'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('VATURNOVER'):
+                        item[key] = netease_item[key]
+                        break
+
+                    if case('VOTURNOVER'):
+                        item[key] = netease_item[key]
+                        break
+                    if case('TURNOVER'):
+                        item[key] = netease_item[key]
+                        break
+                    if case():  # default, could also just omit condition or 'if True'
+                        pass
+                        # No need to break here, it'll stop anyway
+            result.append(item)
+        return result
+
+base = Base()
+print base.getData(code="600007", start_date="2015-01-01", end_date="2019-01-01")
