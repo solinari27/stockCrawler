aac46e94b375570718985ebd879fc79fc8305210
solinari <solinari27@gmail.com>
2019-04-07 20:34:53 +0800

    cal wr

--- index/wr.py
+++ index/wr.py
@@ -6,6 +6,9 @@
 @time: 2018/10/09
 """
 
+# W%R指标即威廉指标，主要是通过分析一段时间内股价最高价、最低价和收盘价之间的关系，
+# 来判断股市的超买超卖现象，预测股价中短期的走势。它主要是利用振荡点来反映市场的超买超卖行为，
+# 分析多空双方力量的对比，从而提出有效的信号来研判市场中短期行为的走势。
 # W%R=(Hn—C)÷(Hn—Ln)×100
 # 其中：C为计算日的收盘价，Ln为N周期内的最低价，Hn为N周期内的最高价，公式中的N为选定的计算时间参数，一般为4或14。
 # 以计算周期为14日为例，其计算过程如下：
@@ -26,22 +29,26 @@ class WR_index():
         default cal index from 1900-01-01
         """
         base = Base()
-        self.datas = base.getData(code=code, start_date=start_date, end_date=end_date)
+        self.datas = base.getData(
+            code=code, start_date=start_date, end_date=end_date)
+        self._index = 0
 
     @property
     def C(self):
-        pass
+        return self.datas[self._index]['TCLOSE']
 
     @property
     def Hn(self):
-        pass
+        Hn = self.datas[self._index]['HIGH']
 
     @property
     def Ln(self):
-        pass
+        Hn = self.datas[self._index]['LOW']
 
     def cal_index(self):
         pass
 
+
 c = WR_index(code="600004")
 c.cal_index()
+print c.C, c.Hn, c.Ln
