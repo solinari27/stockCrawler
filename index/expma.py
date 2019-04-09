d816a9c2fa01fc9d38e7159796c66bffe2e8ccba
solinari <solinari27@gmail.com>
2019-04-09 21:03:56 +0800

    expma

--- index/expma.py
+++ index/expma.py
@@ -13,3 +13,38 @@
 # 同时也在一定程度中消除了DMA指标在某些时候对于价格走势所产生的信号提前性，是一个非常有效的分析指标。
 # EXPMA=（当日或当期收盘价－上一日或上期EXPMA）/N+上一日或上期EXPMA，其中，首次上期EXPMA值为上一期收盘价，N为天数。
 # 经：
+
+import sys
+sys.path.append('/home/ubuntu/stockCrawler')
+sys.path.append('/home/solinari/workspace/stockCrawler')
+
+
+from base import Base
+
+class EXPMA_index():
+
+    def __init__(self, code, start_date="1900-01-01", end_date="2020-01-01"):
+        """
+        default cal index from 1900-01-01
+        """
+        base = Base()
+        self.datas = base.getData(
+            code=code, start_date=start_date, end_date=end_date)
+
+        self._index = 0
+        self._last_expma = 0
+        self.period = 14
+
+        for item in self.datas:
+            print item
+
+
+    def set_period(self, period):
+        self.period = period
+
+
+    def cal_index(self):
+        pass
+
+
+c = EXPMA_index(code="600007")
