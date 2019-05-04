dec3657cf26eaa4108ef1da4cbc326c3554e74d3
solinari <solinari27@gmail.com>
2019-05-04 19:42:36 +0800

    index

--- index/wr.py
+++ index/wr.py
@@ -17,9 +17,9 @@
 # 经：当55/72处于0～20时，强势上涨
 
 
-import sys
-sys.path.append('/home/ubuntu/stockCrawler')
-sys.path.append('/home/solinari/workspace/stockCrawler')
+# import sys
+# sys.path.append('/home/ubuntu/stockCrawler')
+# sys.path.append('/home/solinari/workspace/stockCrawler')
 
 
 import datetime
@@ -95,6 +95,6 @@ class WR_index():
         return wr_list
 
 
-c = WR_index(code="600007")
-c.set_period(period=55)
-print c.cal_index()
+# c = WR_index(code="600007")
+# c.set_period(period=55)
+# print c.cal_index()
