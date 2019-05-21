5c1cf657599d2cc0442c5c2d1985c58e07319512
solinari <deeper1@163.com>
2019-05-21 18:49:42 +0800

    add debug script

---
+++ debug_crawler.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# cd /home/ubuntu/stockCrawler/netease/crawler
+# export PYTHONPATH=/home/ubuntu/stockCrawler:%PYTHONPATH
+cd /home/solinari/workspace/stockCrawler/netease/crawler
+export PYTHONPATH=/home/solinari/workspace/stockCrawler:%PYTHONPATH
+python run.py
