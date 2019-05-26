e806de10cffc3a0f79dec97904842a7c79c0a4e2
solinari <solinari27@gmail.com>
2019-05-26 19:20:30 +0800

    bugfix

--- rundaily.sh
+++ rundaily.sh
@@ -4,4 +4,6 @@ python /home/ubuntu/stockCrawler/crawlliangyeedaily.py
 # python /home/ubuntu/stockCrawler/crawlnetease.py&
 # sh crawlnetease.sh
 # sh crawlsohu.sh
-sudo docker run -d stockcrawler:test /home/ubuntu/stockCrawler/crawlnetease.sh
+# sudo docker run -d stockcrawler:test /home/ubuntu/stockCrawler/crawlnetease.sh
+#docker run -d --net=host stockcrawler /home/solinari/workspace/stockCrawler/crawlnetease.sh
+#docker run -d --net=host stockcrawler /home/solinari/workspace/stockCrawler/crawlsohu.sh
