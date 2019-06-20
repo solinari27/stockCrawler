739de31f2c7792be33681108169de5c8a8b7adb9
solinari <solinari27@gmail.com>
2019-06-20 22:53:32 +0800

    clean docker

--- cleanDisk.sh
+++ cleanDisk.sh
@@ -2,3 +2,4 @@
 cd /home/ubuntu/stockCrawler
 python /home/ubuntu/stockCrawler/cleandb.py&
 
+docker ps -a | sed '/^CONTAINER/d' | grep "Exited" | gawk '{cmd="docker rm "$1; system(cmd)}'docker ps -a | sed '/^CONTAINER/d' | grep "Exited" | gawk '{cmd="docker rm "$1; system(cmd)}'
