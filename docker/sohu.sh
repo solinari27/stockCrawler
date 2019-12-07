7b2f4f6fe2add453767657fba94e8d919bc62824
solinari <solinari27@gmail.com>
2019-12-07 19:27:49 +0800

    bugfix

--- docker/sohu.sh
+++ docker/sohu.sh
@@ -1,1 +1,1 @@
- docker run -v /root/workspace/:/home/solinari/workspace -itd --rm --network=host stockcrawler /home/solinari/workspacce/stockCrawler/crawlsohu.sh
+ docker run -v /root/workspace/:/home/solinari/workspace -itd --rm --network=host stockcrawler /home/solinari/workspace/stockCrawler/crawlsohu.sh
