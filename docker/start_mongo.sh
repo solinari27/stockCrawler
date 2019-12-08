c715b6a84ed9123d9977e246603e0b32144304e0
solinari <solinari27@gmail.com>
2019-12-08 23:12:06 +0800

    mongodb bugfix

--- docker/start_mongo.sh
+++ docker/start_mongo.sh
@@ -1,1 +1,1 @@
- docker run --name mongodb -v /home/docker/db:/data/db -p 27017:27017 --restart=always -d mongo --bind_ip 0.0.0.0
+ docker run --name mongodb -v /home/docker/db:/data/db -p 27017:27017 --restart=always -d mongo --bind_ip 0.0.0.0 --auth
