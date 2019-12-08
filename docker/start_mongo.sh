63e4a5e40aad8d81fb0bfa4d8cedeef57ab94604
solinari <solinari27@gmail.com>
2019-12-08 22:42:11 +0800

    bugfix

--- docker/start_mongo.sh
+++ docker/start_mongo.sh
@@ -1,1 +1,1 @@
- docker run --name mongodb --network=host -v /home/docker/db:/data/db -p 27017:27017 --restart=always -d mongo
+ docker run --name mongodb -v /home/docker/db:/data/db -p 27017:27017 --restart=always -d mongo
