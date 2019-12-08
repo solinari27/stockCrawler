6446c0a731bb2523c8ba584b3ad5c4c8ac9a32dd
solinari <solinari27@gmail.com>
2019-12-08 21:16:51 +0800

    bugfix for docker crawler script

--- docker/sohu.sh
+++ docker/sohu.sh
@@ -1,1 +1,1 @@
- docker run -v /root/workspace/:/home/solinari/workspace -itd --rm --network=host stockcrawler --restart=always /home/solinari/workspace/stockCrawler/crawlsohu.sh
+ docker run -v /root/workspace/:/home/solinari/workspace -itd --rm --network=host --restart=always --name=sohu_crawler stockcrawler /home/solinari/workspace/stockCrawler/crawlsohu.sh
