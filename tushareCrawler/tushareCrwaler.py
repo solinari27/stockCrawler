c7da9f1f755514e0396d4d504391b365d13d8e3c
solinari <solinari27@gmail.com>
2020-05-08 23:05:04 +0800

    a

--- tushareCrawler/tushareCrwaler.py
+++ tushareCrawler/tushareCrwaler.py
@@ -5,3 +5,14 @@
 @file: tushareCrwaler.py 
 @time: 2020/05/07 
 """
+
+# tornado + supervisor + celery
+from datetime import datetime
+import time
+# 每n秒执行一次
+def timer(n):
+    while True:
+        print(datetime.now().strftime("%Y-%m-%d  %H:%M:%S"))
+        time.sleep(n)
+
+timer(5)
